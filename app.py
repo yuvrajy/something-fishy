ec9496407b85526d1fb4866ceda6797d436fbdbc
Cool Kid <37753893+yuvrajy@users.noreply.github.com>
2026-01-31 00:39:00 -0800

    Replace gevent with eventlet monkey patching

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_cors import CORS
@@ -5,11 +8,9 @@ from main import Player, GameRoom
 import random
 import string
 import os
-from gevent import monkey, sleep
 import time
 import threading
 from dotenv import load_dotenv
-monkey.patch_all()
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-production')
@@ -371,7 +372,7 @@ def handle_guess(data):
             emit('game_over', final_results, to=room_code)
         else:
             # Wait for 1.5 seconds to let the animation complete
-            sleep(1.5)
+            eventlet.sleep(1.5)
 
             # end_round() already calls start_new_round(), so we don't need to call it again
             # The round was already ended in process_guess -> end_round()
